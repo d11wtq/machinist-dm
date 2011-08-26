6a22ea089c0223239dea9118095a390f67bf607e
d11wtq <chris@w3style.co.uk>
2011-08-26 22:17:22 +1000

    Working version for 2.0.0-beta2

--- lib/machinist/data_mapper/blueprint.rb
+++ lib/machinist/data_mapper/blueprint.rb
@@ -7,6 +7,18 @@ module Machinist::DataMapper
       object.save && object.reload
     end
 
+    def box(object)
+      object.key
+    end
+
+    def unbox(key)
+      @klass.get(*key)
+    end
+
+    def outside_transaction
+      yield # FIXME: This is not actually implemented
+    end
+
     def lathe_class #:nodoc:
       Machinist::DataMapper::Lathe
     end
